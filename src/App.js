d6d88e121e5c67b6560d9c1bc2e4aa5308199509
Rocío Garrido <rocioganon@gmail.com>
2021-09-06 18:26:11 +0200

    add SignIn function

--- src/App.js
+++ src/App.js
@@ -21,7 +21,33 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
-  return <div className="App">hola</div>;
+  const [user] = useAuthState(auth);
+
+  return (
+    <div className="App">
+      <header></header>
+
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+
+  return (
+    <div>
+      <button className="sign-in" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+      <p>
+        Do not violate the community guidelines or you will be banned for life!
+      </p>
+    </div>
+  );
 }
 
 export default App;
